1acb3d30e9ec886d106144b2b29e2b373b6a1765
KAVIN01-S <104634566+KAVIN01-S@users.noreply.github.com>
2022-09-19 12:25:09 +0530

    Update 4. Implement 2 stacks in single array.java

--- Microsoft/In-Class Assignments/Week - 3/4. Implement 2 stacks in single array.java
+++ Microsoft/In-Class Assignments/Week - 3/4. Implement 2 stacks in single array.java
@@ -1,1 +1,38 @@
-
+class MyQueue {
+    private Stack<Integer> x;
+    private Stack<Integer> y;
+    public MyQueue() {
+        x=new Stack<>();
+        y=new Stack<>();
+    }
+    
+    public void push(int a) {
+        x.push(a);
+    }
+    
+    public int pop() {
+        if(y.isEmpty())
+        {
+            while(!x.isEmpty())
+            {
+                y.push(x.pop());
+            }
+        }
+        return y.pop();
+    }
+    
+    public int peek() {
+        if(y.isEmpty())
+        {
+            while(!x.isEmpty())
+            {
+                y.push(x.pop());
+            }
+        }
+        return y.peek();
+    }
+    
+    public boolean empty() {
+        return x.isEmpty() && y.isEmpty();
+    }
+}
