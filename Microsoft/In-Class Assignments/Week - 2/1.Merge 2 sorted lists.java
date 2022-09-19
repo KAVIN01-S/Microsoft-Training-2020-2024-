33ed96daca35a56f7a61ed3ed48fd2fc60888c37
KAVIN01-S <104634566+KAVIN01-S@users.noreply.github.com>
2022-09-19 12:03:57 +0530

    Update 1.Merge 2 sorted lists.java

--- Microsoft/In-Class Assignments/Week - 2/1.Merge 2 sorted lists.java
+++ Microsoft/In-Class Assignments/Week - 2/1.Merge 2 sorted lists.java
@@ -1,23 +1,25 @@
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-      if(l1==null)
-      {
-          return l2;
-      }  
-      if(l2==null)
-      {
-          return l1;
-      }
-      if(l1.val<=l2.val)
-      {
-          l1.next=mergeTwoLists(l1.next,l2);
-          return l1;
-      }
-      
-        else
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while(list1!=null && list2!=null)
         {
-           l2.next= mergeTwoLists(l1,l2.next);
-          return l2;
+            if(list1.val<=list2.val)
+            {
+                curr.next=list1;
+                list1=list1.next;
+            }
+            else
+            {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
         }
+        curr.next = list1!=null ? list1 : list2;
+        
+        return dummy.next;
     }
 }
