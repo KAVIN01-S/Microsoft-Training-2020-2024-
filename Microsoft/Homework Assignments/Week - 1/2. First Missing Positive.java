d7fb96eea25635a3606791e56fc5e7fae4ca2347
KAVIN S <104634566+KAVIN01-S@users.noreply.github.com>
2022-09-22 19:46:59 +0530

    Update 2. First Missing Positive.java

--- Microsoft/Homework Assignments/Week - 1/2. First Missing Positive.java
+++ Microsoft/Homework Assignments/Week - 1/2. First Missing Positive.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i:nums)
+        {
+            set.add(i);
+        }
+        
+        for(int i=1;i<=nums.length;i++)
+        {
+            if(!set.contains(i))
+            {
+                return i;
+            }
+        }
+        return nums[nums.length-1]+1;
+    }
+}
