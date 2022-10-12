c84c50614438d50944d10aff11294dfe621fd2cc
KAVIN S <104634566+KAVIN01-S@users.noreply.github.com>
2022-10-12 15:12:45 +0530

    Update Design HashMap.java

--- Microsoft/In-Class Assignments/Week - 7/Design HashMap.java
+++ Microsoft/In-Class Assignments/Week - 7/Design HashMap.java
@@ -1,1 +1,19 @@
-
+class MyHashMap {
+    int[] map=null;
+    public MyHashMap() {
+        map=new int[1000001];
+        Arrays.fill(map,-1);
+    }
+    
+    public void put(int key, int value) {
+        map[key]=value;
+    }
+    
+    public int get(int key) {
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        put(key,-1);
+    }
+}
