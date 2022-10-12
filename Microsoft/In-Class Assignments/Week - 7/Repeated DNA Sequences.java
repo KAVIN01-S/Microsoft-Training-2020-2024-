38b93db855ba2eac752712a24bf79d089a9af1b6
KAVIN S <104634566+KAVIN01-S@users.noreply.github.com>
2022-10-12 15:07:58 +0530

    Update Repeated DNA Sequences.java

--- Microsoft/In-Class Assignments/Week - 7/Repeated DNA Sequences.java
+++ Microsoft/In-Class Assignments/Week - 7/Repeated DNA Sequences.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet arr = new HashSet(), dumm = new HashSet();
+        for(int i=0;i<s.length()-9;i++)
+        {
+            String str = s.substring(i,i+10);
+            if(!arr.add(str))
+            {
+                dumm.add(str);
+            }
+        }
+        return new ArrayList(dumm);
+    }
+}
